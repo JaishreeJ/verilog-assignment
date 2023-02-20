27c82e3e51c03771b0aba3fc411564fb67e4b296
JaishreeJ <113256706+JaishreeJ@users.noreply.github.com>
2023-02-20 12:09:12 +0530

    Update clock gate_tb.v

--- clock gating/clock gate_tb.v
+++ clock gating/clock gate_tb.v
@@ -1,30 +1,21 @@
-module clock_gate_tb();
-
-reg clk_i;
-reg en_i;
-wire clk_o;
-
-clock_gate dut(
-  .clk_i(clk_i),
-  .en_i(en_i),
-  .clk_o(clk_o)
-);
-
-initial begin
-  $dumpfile("con11.vcd");
-  $dumpvars;
-  
-  
-  clk_i = 0;
-  en_i = 0;
-  #10;
-  #5 en_i=1;
-  #10 en_i=0;
-  end
-
-  always#5 clk_i=~clk_i;
-
-initial #100  $finish;
-
-
-endmodule
+module clock_gating_tb;
+  reg clk_i;
+  reg en_i;
+ 
+  wire clk_o;
+  clock_gating uut(clk_i,en_i,clk_o);
+  initial
+    begin
+      $dumpfile("clkg.vcd");
+      $dumpvars(1,clock_gating_tb);
+    end
+  always #5 clk_i=~clk_i;
+  initial
+    begin
+      $monitor("time=%t,clk_i=%b,en_i=%b,clk_o=%b",$time,clk_i,en_i,clk_o);
+      en_i=0;clk_i=0;
+      #8 en_i=1;
+      #9 en_i=0;
+      #20$finish;
+    end
+      endmodule
